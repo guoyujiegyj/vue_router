08bd859d78589efed3ef0c19990734b22018c262
gyj <1401357358@qq.com>
2019-11-06 14:10:28 +0800

    通过watch为确定的路由组件设置过渡

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <router-link to="/error"></router-link>
     <!--路由过渡，通过name去设置样式。key是必须的。-->
     <!-- <transition-group name='rou'> -->
-      <transition-group :name='routerTransition'>
+    <transition-group :name='routerTransition'>
       <router-view key="defalut"></router-view>
       <!--命名路由：根据名字进行路由跳转。-->
       <router-link key="search" :to="{name: 'search'}">查找</router-link>
@@ -38,7 +38,16 @@ export default {
     // 监视路由。
     '$route'(to){
       // 只为meta为wmai的组件设置过渡样式。
-      if(to.meta.title==='wmai'){
+      // if(to.meta.title==='wmai'){
+      //   this.routerTransition='rou'
+      // }else{
+      //   this.routerTransition=''
+      // }
+
+      console.log(to.path)
+      // 只为to.path为bools2的设置过渡样式。
+      if(to.path==='/bools2'){
+        
         this.routerTransition='rou'
       }else{
         this.routerTransition=''
